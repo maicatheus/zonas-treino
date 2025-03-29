331b39ba005040b97ec41fb4118c40c46ba23fa6
Matheus Maica <maicatheus@gmail.com>
2025-03-28 23:23:33 -0300

    Adicionando footer

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,11 @@ function App() {
       <h1 className="main-title">Calculadora de Zonas de Treinamento</h1>
       <p className="sub-title">Descubra suas zonas de treino com base na sua idade e aprenda a importância de cada uma para melhorar sua saúde e performance.</p>
       <ZoneCalculator />
+      <footer className="footer">
+        <p>Projeto da Atividade Extensionista II – UNINTER</p>
+        <p>Aluno: Matheus Maica | RU: 3748966</p>
+        <p>Curso: CST em Análise e Desenvolvimento de Sistemas</p>
+      </footer>
     </div>
   )
 }
